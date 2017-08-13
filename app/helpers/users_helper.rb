0e1f7237dcae5e6443cc6cbf19725d63d3f368a3
Tiffany Citra <tiffanycitra95@gmail.com>
2017-08-12 17:12:27 -0700

    finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+	# Returns gravatar for the  given user
+	def gravatar_for(user, size: 50)
+		gid = Digest::MD5::hexdigest(user.email.downcase)
+		gurl = "https://secure.gravatar.com/avatar#{gid}?s=#{size}"
+		image_tag(gurl, alt: user.name, class: "gravatar")
+	end	
 end
